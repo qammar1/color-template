28c9f551c970fa4269224db3bdece4edf566c2a8
qammar1 <qammarshamraiz1@gmail.com>
2024-02-24 11:38:27 +0500

    updated code

--- src/components/SingleColor.jsx
+++ src/components/SingleColor.jsx
@@ -11,8 +11,6 @@ const SingleColor = ({rgb,weight,index}) => {
     <article className={`color`} style={{backgroundColor:hex}}>
   <p className='percent-value'>{weight}%</p>
   <p className='color-value'>{hex}</p>
-  <h1 className='color-value'>{hex}</h1>
-  <h1 className='color-value'>{hex}</h1>
     </article>
   );
 }
